faa53b4a3797abdc3899440e6a09f48454832b8b
Maximiliano Gauna <maximilianog@lagash.com>
2015-07-08 09:05:01 -0300

    Google news incorporadas

--- Pruebas/Pruebas/Controllers/GoogleSpeachApiController.cs
+++ Pruebas/Pruebas/Controllers/GoogleSpeachApiController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +13,14 @@ namespace Pruebas.Controllers
         // GET: GoogleSpeachApi
         public ActionResult Index()
         {
+            WebRequest request = WebRequest.Create(
+                 "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=8&q=http%3A%2F%2Fnews.google.com%2Fnews%3Foutput%3Drss%26pz%3D1%26ned%3Den_us%26hl%3Den%26sort%3Dnewest");
+            //request.Credentials = CredentialCache.DefaultCredentials;
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            ViewBag.GoogleNews = responseFromServer;
             return View();
         }
     }
