b6989088091a923ae5c5263e9a2db54949da33eb
Maximiliano Gauna <maximilianog@lagash.com>
2015-12-01 08:19:40 -0300

    Paquetes de Resultados del lado del Servidor

---
+++ NoMeOlvides/Domain/Hepler/ErrorsPackageContent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Hepler
+{
+    public class ErrorsPackageContent
+    {
+        public bool HasError { get; set; }
+
+        private IList<string> messages;
+        public IList<string> Messages
+        {
+            get
+            {
+                if (messages == null)
+                {
+                    messages = new List<string>();
+                }
+
+                return messages;
+            }
+            set
+            {
+                messages = value;
+            }
+        }
+    }
+}
---
+++ NoMeOlvides/NoMeOlvides.Tests/Helper/ErrorsPackageContentTests.cs
@@ -0,0 +1,117 @@
+﻿using Domain.Hepler;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoMeOlvides.Tests.Helper
+{
+    [TestFixture]
+    public class ErrorsPackageContentTests
+    {
+        private ErrorsPackageContent sut;
+
+        private const int emptyElementsCount = 0;
+        private const string message1 = "message1";
+        private const string message2 = "message2";
+        private readonly IList<string> emptyList = new List<string>();
+        private readonly IList<string> listX1 = new List<string> { message1 };
+        private readonly IList<string> listX2 = new List<string> { message1, message2 };
+
+        [SetUp]
+        public void SetUp()
+        {
+            sut = new ErrorsPackageContent();
+        }
+
+        #region HasError [GET]
+
+        [Test]
+        public void HasError_GET__WithoutPreviousAssignament_ReturnsFalse()
+        {
+
+
+            Assert.IsFalse(sut.HasError);
+        }
+
+        #endregion
+
+        #region HasError [SET]
+
+        [Test]
+        public void HasError_SET__WithFalseValue_ReturnsFalse()
+        {
+
+            sut.HasError = false;
+
+            Assert.IsFalse(sut.HasError);
+        }
+
+        [Test]
+        public void HasError_SET__WithTrueValue_ReturnsTrue()
+        {
+
+            sut.HasError = true;
+
+            Assert.IsTrue(sut.HasError);
+        }
+
+        #endregion
+
+        #region Messages [GET]
+
+        [Test]
+        public void Messages_GET__WithoutAssignament_ReturnsEmptyErrorList()
+        {
+
+
+            Assert.IsNotNull(sut.Messages);
+            Assert.AreEqual(emptyElementsCount, sut.Messages.Count);
+        }
+
+        #endregion
+
+        #region Messages [SET]
+
+        [Test]
+        public void Messages_SET__WithNull_ReturnsEmptyErrorList()
+        {
+
+            sut.Messages = null;
+
+            Assert.IsNotNull(sut.Messages);
+            Assert.AreEqual(emptyElementsCount, sut.Messages.Count);
+        }
+
+        [Test]
+        public void Messages_SET__WithAnEmptyList_ReturnsTheSameList()
+        {
+
+            sut.Messages = emptyList;
+
+            Assert.AreSame(emptyList, sut.Messages);
+        }
+
+        [Test]
+        public void Messages_SET__WithOneElementList_ReturnsTheSameList()
+        {
+
+            sut.Messages = listX1;
+            
+            Assert.AreSame(listX1, sut.Messages);
+        }
+
+        [Test]
+        public void Messages_SET__WithTwoElementsList_ReturnsTheSameList()
+        {
+
+            sut.Messages = listX2;
+
+            Assert.AreSame(listX2, sut.Messages);
+        }
+
+        #endregion
+    }
+}
