48cc363370ed2d35872b289c71173e3626d1e093
Maximiliano Gauna <maxiunlm@gmail.com>
2016-02-03 08:31:19 -0300

    React JS Funcionando + WebCompiler

---
+++ NoMeOlvides/NoMeOlvides/Scripts/BusinessCard/Index.jsx
@@ -0,0 +1,35 @@
+﻿// OLD
+//var CommentBox = React.createClass({
+//    render: function() {
+//        return (
+//          <div className="commentBox">
+//            Hello, world! I am a CommentBox.
+//          </div>
+//      );
+//    }
+//});
+
+//React.render(
+//  <CommentBox />,
+//  document.getElementById('content')
+//);
+
+
+//import React from 'react';
+        
+class CommentBox extends React.Component {
+    render() {
+        return <div className="commentBox">
+            Hello, world! I am a CommentBox.
+          </div>
+        ;
+    }
+}
+
+
+//export default CommentBox;
+
+React.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
